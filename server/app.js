73b78065c61aea66d49aac9016bac05968671731
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-05-23 11:55:20 -0300

    hoy
    hoy

--- server/app.js
+++ server/app.js
@@ -4,12 +4,15 @@ const app = express();
 const PORT = 3000; 
 
 app.get('/', (req, res) =>{
-	res.send (' ')
+	res.send ('Hola Mundo ') //poner mensaje ente comillas
 }) // aca puedo poner caulquier cosa como para ver en el front 
 
+app.get('/hola ', (req, res) =>{ // url a la que uqeremos que vaya (donde esta el hola)
+	res.send ({ messege: 'Hola Mundo!'}) //con res.end no manda nada
+})
 app.listen(PORT, (error) =>{ 
 	if(!error) 
-		console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+		console.log('Server is Successfully Running, and App is listening on port http://localhost + ${PORT}');
 	else
 		console.log("Error occurred, server can't start", error); 
 	} 
