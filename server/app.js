484faa97b743515d7b4cfbce92e013382a96c18f
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-06-05 11:42:52 -0300

    Deploy

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
-const express = require('express'); 
+const express = require('express');
 
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) =>{
-	res.send ('Hola Mundo ') //poner mensaje ente comillas
+app.get('/', (req, res) => {
+	res.send('Hola Mundo ') //poner mensaje ente comillas
 }) // aca puedo poner caulquier cosa como para ver en el front 
 
-app.get('/hola', (req, res) =>{ // url a la que uqeremos que vaya (donde esta el hola)
-	res.send ({ messege: 'Hola Mundo!'}) //con res.end no manda nada
+app.get('/hola', (req, res) => { // url a la que uqeremos que vaya (donde esta el hola)
+	res.send({ messege: 'Hola Mundo!' }) //con res.end no manda nada
 })
 
 
-app.listen(PORT, (error) =>{ 
-	if(!error) 
-		console.log('Server is Successfully Running, and App is listening on port http://localhost + ${PORT}');
+app.listen(PORT, (error) => {
+	if (!error)
+		console.log(`Server is Successfully Running, and App is listening on port ${PORT}`);
 	else
-		console.log("Error occurred, server can't start", error); 
-	} 
-); 
+		console.log("Error occurred, server can't start", error);
+}
+);
