a8388f5bab628ad4daa41c6e4b4510e3fe2683de
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-06-05 11:31:12 -0300

    Preuba

--- tets.js
+++ tets.js
@@ -1,7 +1,7 @@
 const { createServer } = require('node:http');
 const express = require ('express')
 const app = express ()
-const port = 3000;
+const port = 3000; //spesifica en que lugar esta
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +10,5 @@ const server = createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://localhost${port}`);
+  console.log(`Server running at http://localhost${port}`); // significa-> Server running at http://localhost3000
 });
