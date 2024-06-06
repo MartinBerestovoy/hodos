526c9cb00eadb9ce1e3a403a03ab797e0b622013
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-06-06 08:35:09 -0300

    creo que ya esta

--- server/conexion.js
+++ server/conexion.js
@@ -1,8 +1,10 @@
 import mysql from "mysql2";
 
-export const connection = mysql.createConnection({
-    host: "localhost",
+const connection = mysql.createConnection({
+    host: "ep-young-snowflake-a4qgqruy-pooler.us-east-1.aws.neon.tech",
     user: "default",
     password: "fY5TQ2VpSxuP",
     database: "verceldbnm",
 });
+
+export default connection;
