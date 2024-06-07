70b2284a0dadab48517759ebe21178c15f58193e
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-06-07 11:17:34 -0300

    fakta hace la requiest mas lo de la base de datos crear tablita

--- server/conexion.js
+++ server/conexion.js
@@ -1,10 +1,21 @@
-import mysql from "mysql2";
+// import mysql from "mysql2/promise";
 
-const connection = mysql.createConnection({
-    host: "ep-young-snowflake-a4qgqruy-pooler.us-east-1.aws.neon.tech",
-    user: "default",
-    password: "fY5TQ2VpSxuP",
-    database: "verceldbnm",
-});
+// const connection = await mysql.createConnection({
+//     host: "ep-young-snowflake-a4qgqruy-pooler.us-east-1.aws.neon.tech",
+//     user: "default",
+//     password: "fY5TQ2VpSxuP",
+//     database: "verceldb",
+//     port: 5432
+// });
+import pg from 'pg';
+const { Pool } = pg;
 
-export default connection;
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+})
+
+console.log(pool)
+// await client.connect();
+// console.log("Connected to the database");
+
+export default pool;
