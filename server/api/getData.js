630c19abfdd98fb393c07fa828d266da632d265f
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-07-04 09:45:29 -0300

    revivir archivo api, y front conezxtion

---
+++ server/api/getData.js
@@ -0,0 +1,23 @@
+import { Client } from 'pg';
+
+export default async (req, res) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  try {
+    await client.connect();
+
+    const result = await client.query('SELECT materia, promedio* FROM myp'); // tabla que queres usar creoo
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener los datos' });
+  } finally {
+    await client.end();
+  }
+};
