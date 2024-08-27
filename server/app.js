26055eb75557892cbf02bb00e176688526558826
EmmaMorvilloRPK <133154985+EmmaMorvilloRPK@users.noreply.github.com>
2024-08-27 15:58:54 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -117,7 +117,7 @@ function save_AI(req, res) {
 //VerbalReasoning
 //carrer
 	// Realizar la inserción en la base de datos
-	connection.query("INSERT INTO test  (O_score, C_score, E_score, A_score, N_score, NumericalAptitude , SpatialAptitude, PerceptualAptitude, AbstractReasoning, VerbalReasoning, carrer) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [O_score, C_score, E_score, A_score, N_score, NumericalAptitude , SpatialAptitude, PerceptualAptitude, AbstractReasoning, VerbalReasoning, carrer ], (err, result) => {
+	connection.query("INSERT INTO test (O_score, C_score, E_score, A_score, N_score, NumericalAptitude , SpatialAptitude, PerceptualAptitude, AbstractReasoning, VerbalReasoning, carrer) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [O_score, C_score, E_score, A_score, N_score, NumericalAptitude , SpatialAptitude, PerceptualAptitude, AbstractReasoning, VerbalReasoning, carrer ], (err, result) => {
 		if (err) {
 			console.error("Error al guardar la información:", err);
 			res.status(500).send("Error al guardar la información en la base de datos");
