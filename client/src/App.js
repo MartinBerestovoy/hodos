2a363b4349ed905fd53c8d2d6fcbe86c71c48df2
martupirolo <111439509+martupirolo@users.noreply.github.com>
2024-08-27 11:41:49 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './Components/About';
 import Formulario from './Components/Formulario';
-import Prueba from './Components/Prueba';
+import Prueba from './Components/prueba';
 import Inicio from './Components/Inicio';
 
 function App() {
