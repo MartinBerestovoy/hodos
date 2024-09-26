48e24684d740d7e88176fffab18853e2663b4dcf
martupirolo <111439509+martupirolo@users.noreply.github.com>
2024-09-26 14:58:37 -0300

    universidades

--- client/src/Components/Universidades.js
+++ client/src/Components/Universidades.js
@@ -1,26 +1,30 @@
-import React, { useState } from "react"; 
+import React, { useEffect, useState } from "react"; 
+import axios from "axios"; 
 import Navbar from "./Navbar"; 
 import './Universidades.css'; 
 
 const Universidades = () => {
-  const [data, setData] = useState([
-    {
-      area: "Marketing y ventas",
-      universidad: "Udesa",
-    },
-    {
-      area: "Medicina",
-      universidad: "Universidad de Buenos Aires",
-    },
-    {
-      area: "Admin empresas",
-      universidad: "Di Tella",
-    }, ////// PRUEBAAAAA
-  ]); 
-  const [loading, setLoading] = useState(false); 
+  const [data, setData] = useState(null); // Cambiado a null para verificar si hay datos
+  const [loading, setLoading] = useState(true); 
   const [error, setError] = useState(null); 
 
-  
+  // Función para recibir los datos del backend
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://hodos-server.vercel.app/guardar-area_recomendada");
+      setData(response.data); 
+    } catch (error) {
+      console.error("Error al obtener datos:", error); 
+      setError("No se encontraron datos."); // Mensaje de error
+    } finally {
+      setLoading(false); 
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Llama a la función fetchData cuando el componente se monta
+  }, []); // Array vacío para que se ejecute solo una vez
+
   if (loading) {
     return <p className="loading-message">Cargando...</p>; // Mensaje de carga
   }
@@ -29,16 +33,18 @@ const Universidades = () => {
     return <p className="error-message">{error}</p>; // Mensaje de error
   }
 
+  if (!data || !data.areaRecomendada) {
+    return <p className="error-message">No se encontraron datos válidos.</p>; // Manejo de caso sin datos
+  }
+
+  const { area, universidad } = data.areaRecomendada; 
+
   return (
     <>
       <Navbar /> 
       <div className="universidades-container">
-        {data.map((item, index) => (
-          <div key={index}>
-            <h1>Área recomendada: {item.area}</h1> 
-            <h2>Universidad recomendada: {item.universidad}</h2> 
-          </div>
-        ))}
+        <h1>Área recomendada: {area}</h1> 
+        <h2>Universidad recomendada: {universidad}</h2> 
       </div>
     </>
   );
